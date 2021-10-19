50020475a95beea2e75fce16c771bbd92285d731
Fnu Yiliqi <khai.ilch167@gmail.com>
2021-10-19 17:27:24 -0400

    Created Player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] 
+    private float speed = 0.1f;
+    
+    private Animator anim;
+    private Rigidbody2D rb2d;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+        anim.SetFloat("Horizontal",h);
+        anim.SetFloat("Vertical", v);
+        
+        rb2d.MovePosition(transform.position + new Vector3(h, v) * speed);
         
     }
 }
