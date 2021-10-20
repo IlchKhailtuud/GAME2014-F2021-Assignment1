1a95794dd9b98c98f6906b5b2aac14fe4208f109
Fnu Yiliqi <khai.ilch167@gmail.com>
2021-10-20 17:56:29 -0400

    Created player damage effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] 
     private float speed = 0.1f;
 
+    [SerializeField] 
+    private int liveCount;
+    
     private Animator anim;
     private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
+    private Color color;
     private void Awake()
     {
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        color = sr.color;
     }
     
     void Start()
@@ -33,4 +40,27 @@ public class PlayerController : MonoBehaviour
         
         rb2d.MovePosition(transform.position + new Vector3(h, v) * speed);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            liveCount--;
+            StartCoroutine("ShowDamageEffect", 2f);
+        }
+    }
+
+    IEnumerator ShowDamageEffect(float durationCount)
+    {
+        for (int i = 0; i < durationCount * 2; i++)
+        {
+            color.a = 0;
+            sr.color = color;
+            yield return new WaitForSeconds(0.25f);
+            color.a = 1;
+            sr.color = color;
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+    
 }
